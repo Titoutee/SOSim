335e3138b537537063f4fe48d42bf695a51ddef2
Tristan Giga <tristan.giga@free.fr>
2025-04-23 16:00:18 +0200

    Mid

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -1,10 +1,12 @@
 //! Interface for mem structures, including a simulated DRAM bank, CPU registers, paging mechanisms, ...
+use num::{BigUint, Integer, PrimInt};
+
 pub mod config;
 pub mod paging;
 pub mod addr;
 
 /*pub*/ use addr::{Addr, VAddr, _VAddrRawCtxt};
-/*pub*/ use paging::{PageTable, RawPTEntry, FullPTEntry};
+/*pub*/ use paging::{PageTable, RawPTE, FullPTEntry};
 
 pub enum BitMode {
     Bit8,
@@ -12,3 +14,24 @@ pub enum BitMode {
     Bit32,
     Bit64,
 }
+
+trait _From<T> {
+    fn _from(t: T) -> Self;
+}
+
+trait _Into<T> {
+    fn _into(&self) -> T;
+}
+
+impl _From<u32> for bool {
+    fn _from(t: u32) -> Self {
+        if t == 0 {false} else {true}
+    }
+}
+
+impl _Into<u32> for bool {
+    fn _into(&self) -> u32 {
+        if *self { 0b1 } else { 0b0 }
+    }
+}
+
--- src/mem/paging.rs
+++ src/mem/paging.rs
@@ -1,22 +1,20 @@
-use super::{addr::{self, Addr}, config::{self, MemContext}};
-use config::{PTE_PHYS_ADDR_MASK};
+// PTE format does not exactly match the x86_64 standard, as only write and read, as well as address payload is are serialized
+// into the 64b bitset.
 
-pub struct PageTable {
-    // PTEs are hard-indexed, which means the index part (9 bit for 64-bit v-addr) in the v-addr is directly used to access the appropriate PTE
-    arr: [RawPTEntry; 512], // (!)
-}
+use super::{
+    addr::{self, Addr},
+    config::{self, MemContext},
+};
+use config::PTE_PHYS_ADDR_MASK;
+// use num::{BigUint, PrimInt, Integer};
 
-pub struct RawPTEntry {
+pub struct RawPTE {
     bitset: u64, // std bitset for PTE format in x86_64 is 64 bit long
 }
 
-impl RawPTEntry {
+impl RawPTE {
     pub fn new(bitset: u64) -> Self {
-        Self {bitset}
-    }
-
-    fn from_full(full_PTE: FullPTEntry) -> Self {
-        todo!()
+        Self { bitset }
     }
 }
 
@@ -27,27 +25,74 @@ pub struct FullPTEntry {
     phys_addr: u64,
 }
 
-impl FullPTEntry {
-    fn from_raw(r: RawPTEntry) -> Self {
-        let bits = r.bitset;
-        
-        let present = (bits & 0b1).to_bool();
-        let write = ((bits >> 1) & 0b1).to_bool();
-        let read = ((bits >> 2) & 0b1).to_bool();
-        let phys_addr = (bits >> 3) & PTE_PHYS_ADDR_MASK;
+trait _From<T> {
+    fn _from(t: T) -> Self;
+}
 
-        Self {
-            present, write, read, phys_addr,
-        }
+trait _Into<T> {
+    fn _into(&self) -> T;
+}
+
+//// u64 <-> bool 
+//// (!) extend to T: PrimInt
+impl _From<u64> for bool {
+    fn _from(t: u64) -> Self {
+        if t == 0 {return false;}true
     }
 }
 
-pub trait UNSIGNED64Ext {
-    fn to_bool(&self) -> bool;
+impl _Into<u64> for bool {
+    fn _into(&self) -> u64 {
+        if *self {return 0b1} 0b0
+    }
 }
 
-impl UNSIGNED64Ext for u64 {
-    fn to_bool(&self) -> bool {
-        if *self == 0 {false} else {true}
+//// RawPTE <-> FullPTE
+impl From<RawPTE> for FullPTEntry {
+    fn from(r: RawPTE) -> Self {
+        let bits = r.bitset;
+
+        let present = <bool as _From<u64>>::_from(bits & 0b1);
+        let write = <bool as _From<u64>>::_from((bits >> 1) & 0b1);
+        let read = <bool as _From<u64>>::_from((bits >> 2) & 0b1);
+        let phys_addr = (bits >> 3) & PTE_PHYS_ADDR_MASK;
+
+        Self {
+            present,
+            write,
+            read,
+            phys_addr,
+        }
+    }
+}
+////
+
+pub struct PageTable {
+    // PTEs are hard-indexed, which means the index part (9 bit for 64-bit v-addr) in the v-addr is directly used to access the appropriate PTE
+    arr: [RawPTE; 512], // (!)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{_From, _Into};
+
+    #[test]
+    fn bool_from_u64() {
+        let t = <bool as _From<u64>>::_from(1);
+        let f = <bool as _From<u64>>::_from(0);
+        assert!(t && !f);
+    }
+
+    #[test]
+    fn u64_from_bool() {
+        let nt = true._into();
+        let nf = false._into();
+        assert_eq!(nt, 0b1);
+        assert_eq!(nf, 0);
+    }
+
+    #[test]
+    fn fpte_from_raw() {
+
     }
 }
